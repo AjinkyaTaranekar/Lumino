82ee9805c4b0ccd097dac0eb3567b10400f5dacb
Xizhen Luo <xizhenluowork@gmail.com>
2026-02-16 13:18:00 +0000

    polish login and signup form and navbar

--- JobHunt/accounts/views.py
+++ JobHunt/accounts/views.py
@@ -2,10 +2,23 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from .forms import CustomLoginForm
 from .forms import SignUpForm
 
 def home(request):
     return render(request, "home.html")
+
+def login_view(request):
+    if request.method == "POST":
+        form = CustomLoginForm(request, data=request.POST or None)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomLoginForm()
+
+    return render(request, 'registration/login.html', {'form': form})
 
 def signup(request):
     if request.method == "POST":
