987c6c7c778ebbcd272a3ccf027cc254b87afd4d
Xizhen Luo <xizhenluowork@gmail.com>
2026-02-17 13:58:24 +0000

    UIbeautify (#1)
    * polish login and signup form and navbar
    
    * Stitch-applied navbar, home page, roadmap
    
    * job page; profile page

--- JobHunt/accounts/forms.py
+++ JobHunt/accounts/forms.py
@@ -1,10 +1,53 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter username'
+        })
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Email'
+        }),
+        required=True
+    )
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget = forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter password'
+            }
+        )
+        self.fields['password2'].widget = forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirm password'
+            }
+        )
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter username'
+        })
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter password'
+        })
+    )
--- JobHunt/accounts/urls.py
+++ JobHunt/accounts/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path('login/', views.login_view, name='login'),
     path("signup/", views.signup, name="signup"),
     path("profile/", views.profile, name="profile"),
-path("roadmap/", views.roadmap, name="roadmap"),
-path("jobs/", views.job_list, name="jobs"),
+    path("roadmap/", views.roadmap, name="roadmap"),
+    path("jobs/", views.job_list, name="jobs"),
 ]
